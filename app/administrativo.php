6df0ec78ef84262e35b96c51086f61f44c6de994
John Mitysumy <johnmitysumy@gmail.com>
2022-02-11 10:42:58 -0300

    barra de menu adicionada

--- app/administrativo.php
+++ app/administrativo.php
@@ -16,17 +16,15 @@ if(!empty($_SESSION['nivel'] == "0" OR $_SESSION['nivel'] == '1' OR $_SESSION['n
 <!-- LOAD DA PÁGINA -->
  <?php include("style/load/load.php"); ?> 
 <!-- CSS BOOTSTRAP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+  
+<?php include 'view/interface/menu.html';?>
   <title>Boas Vindas.</title>
 </head>
 <body>
 
-<p><h1 align="center"><?php echo "Olá ".$_SESSION['usuario'].", Bem vindo!"; ?> </h1><br></p>
 
-<div align="center" id="sair">
-<a href="sair.php"><button type="button" class="btn btn-primary">SAIR</button></a>
-</div>
+
+
 <br>
 
 
--- app/config/db/login.php
+++ app/config/db/login.php
@@ -2,7 +2,7 @@
 	$servidor = "localhost";
 	$usuario = "root";
 	$senha = "";
-	$dbname = "sistema-login";
+	$dbname = "recepcao";
 	
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);

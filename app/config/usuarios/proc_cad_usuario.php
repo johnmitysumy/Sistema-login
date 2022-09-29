715bfe113aca6840f20d4080f99241fa83ae8187
John Mitysumy <johnmitysumy@gmail.com>
2022-09-29 16:40:26 -0300

    Atribuido uma validação para verificar se está sendo cadastrado um usuário duplicado no sistema.

--- app/config/usuarios/proc_cad_usuario.php
+++ app/config/usuarios/proc_cad_usuario.php
@@ -8,6 +8,19 @@ $usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
 $status = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
 $senha = password_hash( $_POST['senha'], PASSWORD_DEFAULT );
 
+//VERIFICAÇÃO DE USUÁRIO DUPLICADO
+$result_usuarios = "SELECT * FROM login WHERE usuario = '$usuario'";
+$resultado_usuarios = mysqli_query($conn, $result_usuarios);
+while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
+
+	$valor_duplicado = $row_usuario['usuario'];
+}
+if ($valor_duplicado > '') {
+	$_SESSION['msg'] = "USUÁRIO JÁ CADASTRADO!";
+	$_SESSION['msg2'] = "<center>já existe um usuário com este nome de usuário cadastrado no sistema</center>";
+	header("Location: /app/config/usuarios/cadastrar_usuario.php");	
+	}else{
+
 $result_usuario = "
 
 INSERT INTO login (nome, email, usuario, senha, status, nivel ) 
@@ -23,4 +36,5 @@ if(mysqli_affected_rows($conn)){
 	$_SESSION['msg'] = "<p style='color:red;'>Erro!</p>";
 	$_SESSION['msg2'] = "<center><p style='color:red;'>Usuário ".$nome." não foi Cadastrado, Tente novamente.</p></center>";
 	header("Location: /app/config/usuarios/cadastrar_usuario.php");
+}
 }

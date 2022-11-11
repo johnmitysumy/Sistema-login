7713b475f784b1c1ea41482a72a05ccea6aac5ad
John Mitysumy <johnmitysumy@gmail.com>
2022-11-11 15:58:48 -0300

    criada pasta app e adicionado o arquivo db

--- public/app/config/db/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost:3306";
-$usuario = "root";
-$senha = "";
-$dbname = "gestao_logistica";
-
-//Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
